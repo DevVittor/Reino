3955c296f8ed64043d1ea8d7cd374bec974c8cb0
DevVittor <vittorserradev@gmail.com>
2025-06-08 17:43:39 -0300

    Fix: Teste de como vai ficar a rota atual

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,21 +1,47 @@
-import Logo from "../assets/logo.svg"; // se o SVG estiver correto
+// Test.jsx
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";  
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Test() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(
+          "https://reino-production.up.railway.app/api/product/list"
+        );
+        setProducts(response.data.list || []);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="flex gap-3  bg-[#3F2305]">
+    <div className="flex gap-3 bg-[#3F2305]">
       <div className="fixed md:w-[350px] w-[300px] min-h-screen">
         <div className="flex flex-col justify-between items-center gap-10 p-5 min-h-screen bg-[#361500] md:border-none border-r-2 border-[#52280f]">
           <div className="flex justify-center items-center flex-col gap-1 h-1/5 w-full text-center">
-            <img src={Logo} alt="Logo" className="w-24 h-auto" />
-            <h1 className="font-bold text-4xl text-[#FFE99A]">Reino Animal</h1>
+            <Link to="/">
+              <img src={Logo} alt="Logo" className="w-24 rounded-full" />
+            </Link>
+            <Link to="/">
+              <h1 className="font-bold text-4xl text-[#FFE99A]">
+                Reino Animal
+              </h1>
+            </Link>
           </div>
-          <div className=" overflow-y-auto flex-grow w-full flex flex-col gap-3 justify-center items-center">
+
+          <div className="overflow-y-auto flex-grow w-full flex flex-col gap-3 justify-center items-center">
             <div className="w-full">
               <input
                 className="px-3 py-2 rounded-xl w-full bg-[#3F2305] text-[#FFE99A] outline-none border-none font-medium"
                 type="search"
-                name=""
-                id=""
                 placeholder="Buscar produtos..."
               />
             </div>
@@ -32,11 +58,12 @@ export default function Test() {
               </ol>
             </div>
           </div>
+
           <div className="py-2 px-5 bg-[#3F2305] max-h-[100px] rounded-xl w-auto flex justify-center items-center gap-2">
-            <div className="">
+            <div>
               <img
                 className="w-14 h-14 object-cover rounded-full border-2 border-[#361500]"
-                src="https://images.pexels.com/photos/28206842/pexels-photo-28206842/free-photo-of-moda-tendencia-pessoa-mulher.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                src="https://images.pexels.com/photos/28206842/pexels-photo-28206842/free-photo-of-moda-tendencia-pessoa-mulher.jpeg"
                 alt=""
               />
             </div>
@@ -49,13 +76,32 @@ export default function Test() {
           </div>
         </div>
       </div>
-      <div className="flex-grow  md:ml-[350px] grid md:grid-cols-4 grid-cols-2 gap-2 p-2">
-        {Array.from({ length: 30 }).map((_, index) => (
-          <div
-            className="bg-[#361500] h-[250px] w-auto rounded-xl"
-            key={index}
-          ></div>
-        ))}
+
+      <div className="flex-grow flex-col gap-2 justify-center items-center md:ml-[350px]">
+        <div className="bg-red-500 md:h-[400px] h-[250px] p-2 flex justify-center items-center sticky top-0">
+          <div className="w-full h-full bg-red-600"></div>
+        </div>
+        <div className="flex-grow grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 px-2 py-2">
+          {products.map((product) => (
+            <div
+              key={product._id}
+              className="break-inside-avoid flex flex-col justify-center items-center"
+            >
+              <div className="w-full bg-[#361500]">
+                <img
+                  className="w-full h-[150px] object-cover rounded-t-xl"
+                  src={product.cover}
+                  alt={product.name}
+                />
+              </div>
+              <div className="w-full bg-[#070707] rounded-b-xl py-2 px-3">
+                <h2 className="text-[#FFE99A] font-semibold line-clamp-2 leading-5">
+                  {product.name}
+                </h2>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
